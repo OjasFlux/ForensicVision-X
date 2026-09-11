1ffbc002af26e70e0c43c023522a76682e073acf
SUJAN <sujangs8897@gmail.com>
2026-09-11 19:18:44 +0530

    Create generate_gradcam function stub
    Added a placeholder function for Grad-CAM visualization.

---
+++ src/evaluation/gradcam.py
@@ -0,0 +1,16 @@
+def generate_gradcam(
+    model,
+    image_tensor,
+    target_class=None
+):
+    """
+    Generate Grad-CAM visualization.
+
+    Implementation will be added after
+    the baseline model is trained.
+    """
+
+    raise NotImplementedError(
+        "Grad-CAM will be implemented "
+        "after baseline training."
+    )
