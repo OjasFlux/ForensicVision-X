14b48c07f69dba75ca5d12dc5fb477edd160d4cd
SUJAN <sujangs8897@gmail.com>
2026-09-11 19:19:54 +0530

    Create spatial.py

---
+++ src/models/spatial.py
@@ -0,0 +1,11 @@
+from .baseline import ResNet50Baseline
+
+
+class SpatialModel(ResNet50Baseline):
+    """
+    Initial spatial-domain model.
+
+    Currently uses the ResNet50 baseline.
+    """
+
+    pass
