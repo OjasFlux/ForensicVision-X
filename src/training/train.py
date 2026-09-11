fd990374db9335e546fce15fd165902afdcc8281
SUJAN <sujangs8897@gmail.com>
2026-09-11 19:24:41 +0530

    Create train.py

---
+++ src/training/train.py
@@ -0,0 +1,53 @@
+import torch
+
+
+def train_one_epoch(
+    model,
+    dataloader,
+    optimizer,
+    criterion,
+    device
+):
+
+    model.train()
+
+    total_loss = 0.0
+    correct = 0
+    total = 0
+
+    for images, labels in dataloader:
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+
+        loss = criterion(
+            outputs,
+            labels
+        )
+
+        loss.backward()
+
+        optimizer.step()
+
+        total_loss += (
+            loss.item() * images.size(0)
+        )
+
+        predictions = outputs.argmax(
+            dim=1
+        )
+
+        correct += (
+            predictions == labels
+        ).sum().item()
+
+        total += labels.size(0)
+
+    return {
+        "loss": total_loss / total,
+        "accuracy": correct / total
+    }
