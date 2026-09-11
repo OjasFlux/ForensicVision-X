c235b6dc9e663d31c7649c3ed94f2040110bbf1e
SUJAN <sujangs8897@gmail.com>
2026-09-11 19:18:13 +0530

    Create evaluate_cross_generator function
    Added a placeholder function for cross-generator evaluation.

---
+++ src/evaluation/cross_generator.py
@@ -0,0 +1,18 @@
+def evaluate_cross_generator(
+    model,
+    test_loader,
+    generator_name
+):
+    """
+    Evaluate a trained detector on images
+    produced by a generator that was not
+    necessarily present during training.
+    """
+
+    # Implementation will be added once
+    # the dataset and training pipeline exist.
+
+    raise NotImplementedError(
+        "Cross-generator evaluation will be "
+        "implemented in the evaluation phase."
+    )
